7dbb30e7d5a7604b1278364f319dedc35648ef73
Justin Young <jyoung@twilio.com>
2020-02-26 00:52:02 -0600

    fixed page title

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get("/", function(req, res, next) {
-  res.render("index", { title: "DeveloperWeek Austin 2019", connection_status: "connecting...", google_api_key: process.env.GOOGLE_MAPS_API_KEY });
+  res.render("index", { title: "LiveStreaming Questionnaire", connection_status: "connecting...", google_api_key: process.env.GOOGLE_MAPS_API_KEY });
 });
 
 module.exports = router;
